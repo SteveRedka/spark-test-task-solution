2fabedb8fc1d0855248a1fac52f1bf7060ad1a85
Steve Redka <cogitofallacy@gmail.com>
2018-10-07 01:34:35 +0300

    test: rewrite feature test for csv uploader

--- spec/features/csv_uploads_spec.rb
+++ spec/features/csv_uploads_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
-RSpec.feature "Csv uploads", type: :feature do
+RSpec.feature 'Csv uploads', type: :feature do
   let(:admin) { FactoryBot.create(:admin_user) }
+  sample_file_route = Rails.root.join('spec/fixtures/sample.csv')
+
   scenario 'let user upload products from .csv file' do
     sign_in_as! admin
     visit '/admin/products'
     expect(page.body).to include('Import from .csv')
-    attach_file('Image', '/path/to/image.jpg')
+    attach_file(sample_file_route)
     expect do
       click_on('Import from .csv')
-    end.to inclrease { Spree::Product.count }.by(3)
+    end.to change { Spree::Product.count }.by(3)
     expect(page).to have_current_path('/admin/products')
   end
 end
